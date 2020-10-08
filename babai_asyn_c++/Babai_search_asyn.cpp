39bf03f9da5a5cd98639414a1165820298348caf
shilei <lin.shilei@outlook.com>
2020-10-08 13:30:07 -0400

    Commit the C++ version

---
+++ babai_asyn_c++/Babai_search_asyn.cpp
@@ -0,0 +1,49 @@
+//#include "Babai_search_asyn.h"
+#include <iostream>
+//#include <Eigen/Dense>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <fstream>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+int main(){
+	int MAX_JOB = omp_get_max_threads();
+	std::cout<<"Max_threads="<< MAX_JOB <<std::endl;
+
+	int n = 500, nswp = 8, n_jobs = 12;	
+	Babai_search_asyn bsa(n, 1);
+	VectorXd x_ser = bsa.find_raw_x0();
+	for(int i = 0; i <= n_jobs; i++)
+		VectorXd x_par = bsa.find_raw_x0_OMP(n_jobs, i);
+	//std::cout << "X distance=" << (x_par - x_ser).norm() << std::endl;
+
+	//Babai_search_cuda bsa_cuda(n, nswp, 1);
+	//VectorXd x_ser = bsa.find_raw_x0();
+	//std::cout << "X distance=" << (x_par - x_ser).norm() << std::endl;
+
+	
+//	int np = 0, id = 0;
+//	omp_set_num_threads(8);
+//
+//#pragma omp parallel
+//	{
+//		for (int j = 0; j <= 5; j++) {
+//
+//#pragma omp for nowait
+//			for (int i = 5; i >= 0; i--) {
+//				np = omp_get_num_threads();
+//				id = omp_get_thread_num();
+//
+//				printf("Hello from thread % d out of % d threads\n", j, i);
+//			}
+//		}
+//
+//	}
+	
+	return 0;
+	
+}
---
+++ babai_asyn_c++/Babai_search_asyn.h
@@ -0,0 +1,372 @@
+﻿#include <iostream>
+#include <Eigen/Dense>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <random>
+
+#define EPSILON 0.0000001
+
+using namespace std;
+using namespace Eigen;
+
+class Babai_search_asyn {
+public:
+	int n;
+	double init_res, tol;
+	vector<double> R_A, R_sA, y_A, raw_x0;
+	Eigen::MatrixXd A, R;
+	Eigen::VectorXd y, x0;
+
+public:
+	Babai_search_asyn(int n, bool f) {
+		this->n = n;
+
+		std::random_device rd;
+		std::mt19937 gen(rd());  //here you could also set a seed
+		std::normal_distribution<double> dis(-n, n);
+
+		this->x0 = Eigen::VectorXd::Zero(n).unaryExpr([&](int dummy) { return round(dis(gen)); });
+
+		string file_name = "R_" + to_string(n) + ".csv";
+		if (f) {
+			ifstream f(file_name);
+			string row_string, entry;
+			while (getline(f, row_string)) {
+				stringstream row_stream(row_string);
+				while (getline(row_stream, entry, ',')) {
+					double d = stod(entry);
+					if (d != 0) {
+						this->R_sA.push_back(d);
+					}
+					this->R_A.push_back(d);
+				}
+			}
+			this->R = Map<Matrix<double, Dynamic, Dynamic, RowMajor>>(R_A.data(), n, R_A.size() / n);
+		} else {
+			std::mt19937 gen2(rd());
+			std::normal_distribution<double> dis2(-10, 10);;
+			this->A = MatrixXd::Zero(n, n).unaryExpr([&](int dummy) { return dis2(gen2); });
+			this->R = A.householderQr().matrixQR().triangularView<Eigen::Upper>();
+			const static IOFormat CSVFormat(FullPrecision, DontAlignCols, ", ", "\n");
+
+			ofstream file(file_name);
+			if (file.is_open()) {
+				file << R.format(CSVFormat);
+				file.close();
+			}
+
+			ifstream f(file_name);
+			string row_string, entry;
+			while (getline(f, row_string)) {
+				stringstream row_stream(row_string);
+				while (getline(row_stream, entry, ',')) {
+					double d = stod(entry);
+					if (d != 0) {
+						this->R_sA.push_back(d);
+					}
+					this->R_A.push_back(d);
+				}
+			}
+		}
+		std::normal_distribution<double> dis3(-n, n);
+		this->y = R * x0 + 0.5 * Eigen::VectorXd::Zero(n).unaryExpr([&](int dummy) { return round(dis3(gen)); });
+		y_A.resize(y.size());
+		VectorXd::Map(&y_A[0], n) = y;
+
+		this->init_res = (y - R * x0).norm();
+		this->tol = INFINITY;
+
+	}
+
+	VectorXd find_raw_x0_OMP(int n_proc, int nswp) {
+		std::cout << "find_raw_x0_OMP" << std::endl;
+		//std::cout << R_sA.size() << std::endl;
+
+		VectorXd raw_x = VectorXd(n);
+		raw_x = x0;
+
+		vector<double> raw_x_A;
+		raw_x_A.resize(n);
+		VectorXd::Map(&raw_x_A[0], n) = x0;
+
+		double res = INFINITY, sum = 0, time = 0;
+
+		raw_x_A[n - 1] = round(y(n - 1) / R(n - 1, n - 1));
+
+#pragma omp parallel num_threads(n_proc) private(sum) shared(raw_x)
+		{
+			double start = omp_get_wtime();			
+			for (int j = 0; j < nswp; j++) {
+				double sum = 0;
+#pragma omp for nowait //schedule(dynamic, n_proc)
+				for (int i = 1; i < n; i++) {
+#pragma omp parallel reduction(+ : sum)
+					for (int col = n - i; col < n; col++) {
+						//sum += R(k, col) * raw_x(col);
+						//sum += R_A[k * n + col] * raw_x_A[col];
+						sum += R_sA[(n - 1 - i) * n - ((n - 1 - i) * (n - i)) / 2 + col] * raw_x_A[col];
+					}
+					//int k = n - 1 - i;
+					//int f = k * n - (k * (k + 1)) / 2;
+					//int f = (n - 1 - i) * n - ((n - 1 - i) * (n - i)) / 2;
+					//#pragma omp parallel reduction(+ : sum)
+					//raw_x_A[k] = round((y_A[k] - sum) / R(k, k));
+					//raw_x_A[k] = round((y_A[k] - sum) / R_A[k * n + k]);
+					raw_x_A[n - 1 - i] = round((y_A[n - 1 - i] - sum) / R_sA[(n - 1 - i) * n - ((n - 1 - i) * (n - i)) / 2 + n - 1 - i]);
+					sum = 0;
+				}
+			}
+			time = omp_get_wtime() - start;
+		}
+
+
+		VectorXd x_result = VectorXd(n);
+		for (int i = 0; i < n; i++) {
+			x_result(i) = raw_x_A[i];
+		}
+
+		res = (y - R * x_result).norm();
+		printf("\\item For %d \"sweeps\", the residual is %.5f, and the running time is %f seconds \n", nswp, res, time);
+
+		return x_result;
+	}
+
+
+
+
+	VectorXd find_raw_x0() {
+
+		double res;
+		std::cout << "find_raw_x0" << std::endl;
+		//std::cout << x0 << endl;
+
+		double s = y(n - 1) / R(n - 1, n - 1);
+		double sum = 0, sum1 = 0;
+		vector<double> raw_x0;
+		raw_x0.resize(n);
+		VectorXd::Map(&raw_x0[0], n) = x0;
+		//VectorXd raw_x0 = x0;
+		raw_x0[n - 1] = round(s);
+
+		double start = omp_get_wtime();
+		for (int k = n - 2; k >= 0; k--) {
+			for (int col = k + 1; col < n; col++) {
+				sum += R_A[k * n + col] * raw_x0[col];
+				//int f = k * n + col - (k * (k + 1)) / 2;
+				//sum += R_sA[f] * raw_x0[col];
+				//if(f>= 500500)
+				//	std::cout << f << endl;
+			}
+			//VectorXd d = R.block(k, k + 1, 1, n - k - 1) * raw_x0.block(k + 1, 0, n - k - 1, 1);
+			//std::cout << sum - sum1 << endl;
+			//s = (y_A[k] - d(0)) / R_sA[k * n + k - (k * (k + 1)) / 2];
+			//s = (y(k) -sum) / R(k, k);
+			s = (y_A[k] - sum) / R_sA[k * n + k - (k * (k + 1)) / 2];
+			raw_x0[k] = round(s);
+			sum = 0;
+			//sum1 = 0;
+		}
+		double end = omp_get_wtime();
+
+		VectorXd x_result = VectorXd(n);
+		for (int i = 0; i < n; i++) {
+			x_result(i) = raw_x0[i];
+		}
+
+		res = (y - R * x_result).norm();
+		this->tol = res;
+		printf("For %d steps, res = %.5f, init_res = %.5f %f seconds\n", n, res, this->init_res, end - start);
+		return x_result;
+	}
+};
+
+//	VectorXd find_raw_x0_OMP_test(int n_proc, int nswp) {
+//		std::cout << "find_raw_x0_OMP" << std::endl;
+//
+//		VectorXd raw_x = VectorXd(n);
+//		raw_x = x0;
+//
+//		vector<double> raw_x_A;
+//		raw_x_A.resize(n);
+//		VectorXd::Map(&raw_x_A[0], n) = x0;
+//		double res = INFINITY, sum = 0;
+//
+//		double start = omp_get_wtime();
+//
+//#pragma omp parallel num_threads(n_proc) private(sum) shared(raw_x)
+//		{
+//			for (int j = 0; j < nswp; j++) {
+//#pragma omp for nowait schedule(dynamic, n_proc*nswp)
+//				for (int i = 0; i < n; i++) {
+//					int k = n - i - 1;
+//					double sum = 0;
+//					if (k != n - 1) {
+//						//#pragma omp parallel reduction(+ : sum)
+//						for (int col = k + 1; col < n; col++) {
+//							//sum += R(k, col) * raw_x(col);
+//							//sum += R_A[k * n + col] * raw_x_A[col];
+//							int f = k * n + col - (k * (k + 1)) / 2;
+//							sum += R_sA[f] * raw_x0[col];
+//						}
+//					}
+//					//raw_x_A[k] = round((y_A[k] - sum) / R(k, k));
+//					raw_x_A[k] = round((y_A[k] - sum) / R_sA[(k + 1) * n - (k * (k + 1)) / 2]);
+//				}
+//			}
+//		}
+//		double end = omp_get_wtime();
+//
+//		VectorXd x_result = VectorXd(n);
+//		for (int i = 0; i < n; i++) {
+//			x_result(i) = raw_x_A[i];
+//		}
+//
+//		res = (y - R * x_result).norm();
+//		printf("For %d steps, res = %.5f, init_res = %.5f %f seconds \n", nswp * n, res, this->init_res, end - start);
+//
+//		return x_result;
+//	}
+	//#pragma omp parallel num_threads(n_proc) private(sum) shared(raw_x)
+	//		{
+	//			//cudaSetDevice(cpuid);
+	//			double start = omp_get_wtime();
+	//			for (int j = 0; j < nswp; j++) {
+	//				int id = omp_get_thread_num();
+	//
+	//				//VectorXd raw_x_local = raw_x;
+	//#pragma omp for nowait 
+	//				for (int i = 0; i < n; i++) {
+					//	int k = n - i - 1;
+					//	double t = y(i);
+					//	for (int l = IA(i); l < IA(i + 1); l++) {
+					//		if (l == i) {
+					//			d = R(j);
+					//		} else {
+					//			t = t - R(j) * raw_x(jA(j));
+					//		}
+					//	}
+					//	raw_x(i) = round((t / d);
+					//}
+					//}
+					//std::cout << omp_get_thread_num() << " " << j << ":" << i << std::endl;
+					////if (j % n == i && cur_j[k + 1] == 1 && cur_j[k] == 0) {									
+					//int k = n - i;
+					//double sum = 0;
+					//if (k != n - 1) {
+					//	for (int col = k + 1; col < n; col++) {
+					//		sum += R(k, col) * raw_x(col);
+					//		//raw_x(col, 0) = round((y(k) - R(k, col) * raw_x(col, 0)) / R(k, k));
+					//	}
+					//	//VectorXd d = R.block(k, k + 1, 1, n - k - 1) * raw_x.block(k + 1, j, n - k - 1, 1);
+					//}
+					////VectorXd d = R.block(k, k + 1, 1, n - k - 1) * raw_x.block(k + 1, 0, n - k - 1, 1);
+					////std::cout << d(0) - s<<" ";
+					//raw_x(k) = round((y(k) - sum) / R(k, k));
+				//}
+				//#pragma omp single
+				//				{
+				//					//res = (y - R * raw_x.block(0, 0, n, 1)).norm();					
+				//					//res = (y - R * raw_x).norm();
+				//					//if (res - tol < 0.1) {
+				//					//	//std::cout << res << endl;
+				//					//	std::cout << j << endl;
+				//					//	//std::cout << omp_get_thread_num() << endl;
+				//					double end = omp_get_wtime();
+				//					printf("Work took %f seconds\n", end - start);
+				//					//	abort_index = j;
+				//					//	abort = 1;
+				//					//}
+				//				}
+								//DWORD time = GetTickCount() - start;
+								//std::cout << time << endl;
+			//}
+		//}
+		//		{
+		//			for (int p = 0; p <= n_proc; p++) {
+		//				while (res > tol) {
+		//#pragma omp 
+		//				}
+		//			}
+		//for (int j = 0; j < nswp; j++) {
+			//#pragma omp for nowait
+//		for (int i = 0; i < n; i++) {
+//
+//			//for (int k = n - 1; k >= 0; k--) {
+//			//std::cout << omp_get_thread_num() <<" "<< j % n << ":" << cur_j[k + 1] << ":" << cur_j[k] << std::endl;
+//			//if (j % n == i && cur_j[k + 1] == 1 && cur_j[k] == 0) {
+//			double s = 0;
+//#pragma omp parallel
+//			{
+//				int k = n - i - 1;
+//				
+//				if (k == n - 1) {
+//					s = y(n - 1) / R(n - 1, n - 1);
+//				} else {
+//
+//#pragma omp for reduction(+ : s)
+//					for (int col = k + 1; col < n; col++) {
+//						s += R(k, col) * raw_x(col, 0);
+//						//raw_x(col, 0) = round((y(k) - R(k, col) * raw_x(col, 0)) / R(k, k));
+//					}
+//					s = (y(k) - s) / R(k, k);
+//				}
+//				//VectorXd d = R.block(k, k + 1, 1, n - k - 1) * raw_x.block(k + 1, 0, n - k - 1, 1);
+//				//std::cout << d(0) - s<<" ";
+//				raw_x(k, 0) = round(s);
+//			}
+//
+//		}
+		//}
+	//}
+	//}
+//}
+//cur_j[k]++;
+//printf("%d\n", k);
+//}
+//cur_j(k)++;
+//if (k == n - 1) {
+//	s = y(n - 1) / R(n - 1, n - 1);
+//} else {
+//	d = R.block(k, k + 1, 1, n - k - 1) * raw_x.block(k + 1, j, n - k - 1, 1);
+//	s = (y(k) - d(0)) / R(k, k);
+//}
+//raw_x(k, j + 1) = round(s);
+//#pragma omp single
+//					{
+//						res = (y - R * raw_x.block(0, j, n, 1)).norm();
+//						printf("res = %f\n", res);
+//						
+//					}
+//std::cout << omp_get_thread_num() << " j(" << j << ") = " << cur_j[j] << " " << endl;
+//printf(" next_j = %d, id = %d\n", j + 1, omp_get_thread_num());
+//#pragma omp single
+//				{
+//					res = (y - R * raw_x.block(0, j, n, 1)).norm();
+//					printf("res = %f\n", res);
+//				}
+//}
+//#pragma omp critical
+//{
+//	//DWORD time = GetTickCount() - start;
+//	//res = (y - R * raw_x.block(0, nswp, n, 1)).norm();
+//	//printf("res = %.5f, %d milliseconds \n", res, time);
+//}
+		//res = (y - R * raw_x.block(0, nswp, n, 1)).norm();
+		//printf("For %d steps, res = %.5f, init_res = %.5f %d milliseconds \n", nswp * n, res, this->init_res, time);
+		//for (j = 2; j <= nswp; j++) {
+		//	/*for (int i = 0; i < n; i++) {
+		//		raw_x(i, j) = round(raw_x(i, j));
+		//	}*/
+		//	std::cout << j << " " << (y - R * raw_x.block(0, j, n, 1)).norm() << std::endl;
+		//}
+		//VectorXd ret_x = raw_x.block(0, 0, n, 1);
+		//res = (y - R * ret_x).norm();
+	//	res = (y - R * raw_x).norm();
+	//	printf("For %d steps, res = %.5f, init_res = %.5f %f seconds \n", nswp * n, res, this->init_res, end - start);
+	//	return raw_x;
+	//}
