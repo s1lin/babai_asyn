d9751be61a1ca13cac765009fa53b3467181b5bf
s1lin <lin.shilei@outlook.com>
2020-12-15 23:00:18 -0500

    Try new data structure

--- babai_asyn_cluster/src/example/ils_block_search.cpp
+++ babai_asyn_cluster/src/example/ils_block_search.cpp
@@ -68,7 +68,7 @@ void ils_block_search(index k, index SNR) {
 //                res = cils::find_residual<scalar, index, n>(cils.R_A, cils.y_A, &reT.x);
 //                ber = cils::find_bit_error_rate<scalar, index, n>(&reT.x, &cils.x_t, cils.qam == 1);
 //                printf("Method: ILS_OMP_NON, n_proc: %d, Res: %.5f, BER: %.5f, Num_iter: %d, Time: %.5fs, SpeedUp: %.3f\n",
-                       n_proc, res, ber, reT.num_iter, reT.run_time, (ils_tim / reT.run_time));
+//                       n_proc, res, ber, reT.num_iter, reT.run_time, (ils_tim / reT.run_time));
             }
 
         }
