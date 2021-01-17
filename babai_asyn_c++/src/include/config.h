d3e4da7971cedccf7144b1099be808e5254a1fbb
shilei_ubuntu <lin.shilei@outlook.com>
2021-01-16 21:54:38 -0500

    QR Code move to cils_reduction.cpp

--- babai_asyn_c++/src/include/config.h
+++ babai_asyn_c++/src/include/config.h
@@ -20,7 +20,7 @@ namespace cils {
 
         index k = 3;
         index SNR = 55;
-        index max_iter = 1;
+        index max_iter = 10;
         index search_iter = 3;
         index stop = 1000;
         index schedule = 1;
