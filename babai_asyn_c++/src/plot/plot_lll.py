fb3b5d13add01e66e653233b1250ecac6595a6d1
shilei-mac <lin.shilei@outlook.com>
2022-04-13 10:37:47 -0400

    Improved Plot methods

--- babai_asyn_c++/src/plot/plot_lll.py
+++ babai_asyn_c++/src/plot/plot_lll.py
@@ -16,18 +16,18 @@ def plot_lll(n, i, max_proc, min_proc, qrT, asplT, totalT):
     marker = ['o', '+', 'x', '*', '>']
     linestyle = ['-.', '-']
     # ax_zoom = fig.add_axes([0.52, 0.51, 0.12, 0.3])
-
     # proc_num = proc_num.astype(int)
     itr_label = ['SEQ'] + ['NT-' + str(proc) for proc in range(min_proc, max_proc + 1, min_proc)]
     d = 0
-    labels = [r'$A\in\mathbb{R}^{40\times40}$', r'$A\in\mathbb{R}^{80\times80}$', r'$A\in\mathbb{R}^{120\times120}$',
-              r'$A\in\mathbb{R}^{160\times160}$', r'$A\in\mathbb{R}^{200\times200}$']
-    for dim in range(40, 201, 40):
+    labels = [r'$\mathbf{A}\in\mathbb{R}^{40\times40}$', r'$\mathbf{A}\in\mathbb{R}^{80\times80}$',
+              r'$\mathbf{A}\in\mathbb{R}^{120\times120}$',
+              r'$\mathbf{A}\in\mathbb{R}^{160\times160}$', r'$\mathbf{A}\in\mathbb{R}^{200\times200}$']
+    for dim in range(40, 41, 40):
 
         for k in range(0, 2):
-            axes[k, 0].set_title('Solve Time', fontsize=13)
-            axes[k, 1].set_title('Solve Time (log)', fontsize=13)
-            axes[k, 2].set_title('Speed Up', fontsize=13)
+            axes[k, 0].set_title(f'Case {k + 1}: Solve Time', fontsize=13)
+            axes[k, 1].set_title(f'Case {k + 1}: Solve Time (log)', fontsize=13)
+            axes[k, 2].set_title(f'Case {k + 1}: Speed Up', fontsize=13)
 
             axes[k, 0].set_ylabel('Solve Time (s)', fontsize=13)
             axes[k, 1].set_ylabel('Solve Time (log)', fontsize=13)
@@ -35,17 +35,27 @@ def plot_lll(n, i, max_proc, min_proc, qrT, asplT, totalT):
 
             a_t = []
             spu = []
-
+            print(qrT)
+            print(qrT[0][:])
             for t in range(0, i + 1):
                 for l in range(0, len(itr_label)):
-                    a_t.append(0)
-                    a_t[l] = a_t[l] + asplT[d][t][l][k]
+                    if t == 0:
+                        a_t.append(0)
+                    a_t[l] = a_t[l] + qrT[d][t][l][k]
 
-            for l in range(1, len(itr_label)):
-                spu.append(a_t[0] / a_t[l])
+                    if l > 0:
+                        if t == 0:
+                            spu.append(0)
+                        spu[l - 1] = spu[l - 1] + qrT[d][t][0][k] / qrT[d][t][l][k]
+
+            for l in range(0, len(itr_label)):
+                a_t[l] = a_t[l] / i
+
+            for l in range(0, len(itr_label) - 1):
+                spu[l] = spu[l] / i
 
             # print(a_t)
-            if k==0:
+            if k == 0:
                 axes[k, 0].plot(itr_label, np.array(a_t), color=color[d], marker=marker[d], label=labels[d])
             else:
                 axes[k, 0].plot(itr_label, np.array(a_t), color=color[d], marker=marker[d])
@@ -70,7 +80,7 @@ def plot_lll(n, i, max_proc, min_proc, qrT, asplT, totalT):
     #                  color=color[1], marker=marker[1])
     # ax_zoom_title = itr_label[1] + ' ' + itr_label[3] + ' ' + itr_label[5] + ' Zoom'
     # ax_zoom.set_title(ax_zoom_title, fontsize=13)
-    title = 'Solve Time with Speed Up for \n ASPL' + str(n)
+    title = 'Runtime performance of the ASPL algorithm and PASPL algorithm.\n'
 
     fig.suptitle(title, fontsize=15)
     fig.legend(bbox_to_anchor=(1, 1), title="Legend", ncol=5)
@@ -81,8 +91,9 @@ def plot_lll(n, i, max_proc, min_proc, qrT, asplT, totalT):
 
 if __name__ == "__main__":
     n = 5
-    i = 0
-    a = np.load(f'../../cmake-build-debug/test_result/{n}_report_plot_{int(i / 200)}_ASPL.npz')
+    a = np.load(f'./test_result/{n}_report_plot_0_ASPL.npz')
+    i = a['i']
+    print(i)
     max_proc = a['max_proc']
     min_proc = a['min_proc']
     qrT = a['qrT']
