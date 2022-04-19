053232df16a968311387c984615eb4da01ef7aae
linsh <lin.shilei@outlook.com>
2022-04-19 16:59:49 -0400

    Modify cils_LLL_test.cpp and plot_lll.py

--- babai_asyn_c++/main.cpp
+++ babai_asyn_c++/main.cpp
@@ -1,7 +1,7 @@
 //#include "src/example/cils_standard_test.cpp"
 //#include "src/example/cils_underdetermined_test.cpp"
-//#include "src/example/cils_LLL_test.cpp"
-#include "src/example/cils_PBNP_test.cpp"
+#include "src/example/cils_LLL_test.cpp"
+//#include "src/example/cils_PBNP_test.cpp"
 
 using namespace std;
 using namespace cils;
@@ -37,11 +37,11 @@ int main(int argc, char *argv[]) {
     printf("\n====================[ Run | cils | Release ]==================================\n");
     double t = omp_get_wtime();
 
-    int start = stoi(argv[1]);
-    int end = stoi(argv[2]);
+//    int start = stoi(argv[1]);
+//    int end = stoi(argv[2]);
 //    CILS cils = cils_driver<double, int>(argc, argv);
-//    plot_LLL<double, int>();
-    test_PBNP<double, int>(start, end);
+    plot_LLL<double, int>();
+//    test_PBNP<double, int>(start, end);
 
     t = omp_get_wtime() - t;
 
