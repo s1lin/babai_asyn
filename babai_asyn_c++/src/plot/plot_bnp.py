98df3d2f006f0731eb2070f1cf3ad5a3fb8b2610
shilei-mac <lin.shilei@outlook.com>
2022-04-23 22:53:57 -0400

    Improved Plot methods

--- babai_asyn_c++/src/plot/plot_bnp.py
+++ babai_asyn_c++/src/plot/plot_bnp.py
@@ -14,193 +14,82 @@ def save_data(n, i, start, end, qrT, asplT, bnp, ber, itr):
              n=n, i=i, start=start, end=end, qrT=qrT, asplT=asplT, bnp=bnp, ber=ber, itr=itr)
 
 
-def plot_bnp(n, i, start, end, qrT, asplT, bnp, ber, itr):
-    print("\n----------PLOT RUNTIME--------------\n")
-    color = ['r', 'g', 'b', 'm']
-    marker = ['o', '+', 'x', '*']
+def plot_bnp():
+    print("\n----------PLOT SNRBER--------------\n")
+    color = ['r', 'g', 'b', 'm', 'tab:orange', 'r']
+    marker = ['o', '>', 'x', '*', '+', '<']
     procs = [5, 10, 15, 20]
-    labels = [r'$\mathbf{z}^{(0)} = \mathbf{0}$', r'$\mathbf{z}^{(0)} = avg$',
-              r'$\mathbf{z}^{(0)} = \lfloor\mathbf{z}_r\rceil$', 'Reduction']
-    itr_label = ['$1$'] + ['$' + str(proc) + '$' for proc in procs]
+    linestyle = ['-.', '-']
 
-    # a = np.load(f'./test_result/{n}_report_plot_200_BNP.npz')
-    # i = a['i']
-    # ber = a['ber']
-    # qrT = a['qrT']
-    # asplT = a['asplT']
+    itr_label = ['$1$-BNP'] + ['$' + str(proc) + '$' for proc in procs]
+    snr_label = ['0', '10', '20', '30', '40', '50']
+    plt.rcParams["figure.figsize"] = (14, 16)
+    fig, axes = plt.subplots(3, 2, constrained_layout=True)
 
-    for j in range(0, 2):
-        if j == 0:
-            plt.rcParams["figure.figsize"] = (20, 16)
-            fig, axes = plt.subplots(3, 2, constrained_layout=True)
-        else:
-            plt2.rcParams["figure.figsize"] = (20, 16)
-            fig, axes = plt2.subplots(3, 2, constrained_layout=True)
+    plt2.rcParams["figure.figsize"] = (20, 16)
+    fig2, axes2 = plt2.subplots(3, 2, constrained_layout=True)
 
-        qam = 4 if j == 0 else 64
-        axes[0, 1].set_title('Avg. Iterations vs Number of Cores', fontweight="bold")
-        axes[0, 0].set_title('Avg. BER vs Number of Cores', fontweight="bold")
-        axes[1, 0].set_title('Avg. Time vs Number of Cores', fontweight="bold")
-        axes[1, 1].set_title('Speed Up vs Number of Cores', fontweight="bold")
-        axes[2, 0].set_title('Avg. Time with Reduction vs Number of Cores', fontweight="bold")
-        axes[2, 1].set_title('Total Speed Up vs Number of Cores', fontweight="bold")
+    sizes = [[50, 150], [250, 350], [450, 550]]
+    SNRs = [[0, 10], [20, 30], [40, 50]]
+    nns = [50, 150, 250, 350, 450, 550]
+    # SNR_BER
 
-        axes[0, 1].set_ylabel('Avg. Iterations', fontweight="bold")
-        axes[0, 0].set_ylabel('Avg. BER', fontweight="bold")
-        axes[1, 0].set_ylabel('Avg. Time (seconds)', fontweight="bold")
-        axes[1, 1].set_ylabel('Speed Up', fontweight="bold")
-        axes[2, 0].set_ylabel('Avg. Time (seconds)', fontweight="bold")
-        axes[2, 1].set_ylabel('Speed Up', fontweight="bold")
+    labels_snr = ['1-BNP', '$n_c=5$', '$n_c=10$', '$n_c=15$', '$n_c=20$']
+    for f in range(0, 3):
+        for ff in range(0, 2):
+            n = sizes[f][ff]
+            data = np.load(f'./test_result/{n}_report_plot_0_0_BNP.npz')
+            i = data['i']
+            ber = data['ber']
 
-        # reduction:
-        a_t = []
-        spu = []
-        spu2 = []
-        for t in range(0, i + 1):
-            total = []
-            for l in range(0, len(itr_label)):
-                print(qrT[t][l][j])
-                if t == 0:
-                    a_t.append(0)
-                # if l == len(itr_label) - 1:
-                #     print(totalT[d][t][l][j], totalT[d][t][l+1][j], totalT[d][t][l+2][j])
-                #     a_t[l] = a_t[l] + min(totalT[d][t][l][j], totalT[d][t][l+1][j], totalT[d][t][l+2][j])
-                # else:
-                # if l == len(itr_label) - 1:
-                total.append(asplT[t][l][j] + qrT[t][l][j])
-                # else:
-                #     total.append(min(asplT[t][l][j], asplT[t][l + 1][j]) + \
-                #                  min(qrT[t][l][j], qrT[t][l + 1][j]))
-
-                a_t[l] = a_t[l] + total[l]
-
-                if l > 0:
-                    if t == 0:
-                        spu.append(0)
-                        spu2.append(0)
-
-                    spu[l - 1] = spu[l - 1] + qrT[t][0][j] / qrT[t][l][j]
-                    spu2[l - 1] = spu2[l - 1] + total[0] / total[l]
-
-        for x in range(0, 3):  # init
-            omp_itr = []
-            omp_ber = []
-            omp_red = []
-            omp_stm = []
-            omp_spu = []
-            omp_spu2 = []
-            omp_total = []
-            for t in range(0, i + 1):
-                to0 = qrT[t][0][j] + asplT[t][0][j] + bnp[x][t][0][j]
-                for l in range(0, len(itr_label)):
-
-                    if t == 0:
-                        omp_itr.append(0)
+            for j in range(0, 2):
+                for l in range(0, 5):
+                    omp_ber = []
+                    for snr in range(0, len(snr_label)):
                         omp_ber.append(0)
-                        omp_red.append(0)
-                        omp_stm.append(0)
-                        omp_total.append(0)
-                    # if l == len(itr_label) - 1:
-                    #     print(totalT[d][t][l][j], totalT[d][t][l+1][j], totalT[d][t][l+2][j])
-                    #     a_t[l] = a_t[l] + min(totalT[d][t][l][j], totalT[d][t][l+1][j], totalT[d][t][l+2][j])
-                    # else:
-                    omp_itr[l] = omp_itr[l] + itr[x][t][l][j]
-                    omp_ber[l] = omp_ber[l] + ber[x][t][l][j]
-
-                    tot = qrT[t][l][j] + asplT[t][l][j] + bnp[x][t][l][j]
-
-                    if l == len(itr_label) - 1:
-                        omp_stm[l] = omp_stm[l] + min(bnp[x][t][l][j], bnp[x][t][l + 1][j], bnp[x][t][l + 2][j])
-                        tot = min(qrT[t][l][j], qrT[t][l + 1][j]) + \
-                              min(asplT[t][l][j], asplT[t][l + 1][j]) + \
-                              min(bnp[x][t][l][j], bnp[x][t][l + 1][j], bnp[x][t][l + 2][j])
+                        for t in range(0, i):
+                            omp_ber[snr] = omp_ber[snr] + ber[snr][t][l][j] / i
+                    if j == 0 and f == 0 and ff == 0:
+                        axes[f, ff].plot(snr_label, omp_ber, color=color[l], marker=marker[l], label=labels_snr[l],
+                                         markersize=12)
+                    elif j == 0:
+                        axes[f, ff].plot(snr_label, omp_ber, color=color[l], marker=marker[l], markersize=12)
                     else:
-                        omp_stm[l] = omp_stm[l] + bnp[x][t][l][j]
+                        axes[f, ff].plot(snr_label, omp_ber, color=color[l], marker=marker[l], linestyle='-.',
+                                         markersize=12)
+    print(snr_label)
+    for f in range(0, 3):
+        for ff in range(0, 2):
+            axes[f, ff].set_title(f'Avg. BER vs SNR for Dimension {sizes[f][ff]}', fontweight="bold")
+            axes[f, ff].set_ylabel('Avg. BER', fontweight="bold")
+            axes[f, ff].set_xticklabels(snr_label)
+            axes[f, ff].set_xlabel('SNR', fontweight="bold")
+            axes[f, ff].grid(color='b', ls='-.', lw=0.25)
+            axes[f, ff].patch.set_edgecolor('black')
+            axes[f, ff].patch.set_linewidth('1')
 
-                    omp_total[l] = omp_total[l] + tot
+            # axes2[f, ff].set_title(f'Avg. Speedup vs Number of Cores for {SNRs[f][ff]}-SNR', fontweight="bold")
+            # axes2[f, ff].set_ylabel('Avg. Speedup', fontweight="bold")
+            # axes2[f, ff].set_xticklabels(itr_label)
+            # axes2[f, ff].grid(color='b', ls='-.', lw=0.25)
+            # axes2[f, ff].patch.set_edgecolor('black')
+            # axes2[f, ff].patch.set_linewidth('1')
 
-                    if l > 0:
-                        if t == 0:
-                            omp_spu.append(0)
-                            omp_spu2.append(0)
-                        if l == len(itr_label) - 1:
-                            omp_spu[l - 1] = omp_spu[l - 1] + bnp[x][t][0][j] / min(bnp[x][t][l][j],
-                                                                                    bnp[x][t][l + 1][j], bnp[x][t][l + 2][j])
-                        else:
-                            omp_spu[l - 1] = omp_spu[l - 1] + bnp[x][t][0][j] / bnp[x][t][l][j]
-                        omp_spu2[l - 1] = omp_spu2[l - 1] + to0 / tot
+    fig.suptitle("\n\n\n\n\n")
+    fig.legend(bbox_to_anchor=(0.88, 0.97), title="Legend", ncol=5, fontsize=21, title_fontsize=21,
+               edgecolor='black')
 
+    plt.savefig(f'./report_plot_SNR_BER_BNP_1.png')
+    plt.savefig(f'./report_plot_SNR_BER_BNP_1.eps', format='eps', dpi=1200)
+    plt.close()
 
-            for l in range(0, len(itr_label)):
-                omp_itr[l] = omp_itr[l] / (i + 1)
-                omp_ber[l] = omp_ber[l] / (i + 1)
-                omp_red[l] = omp_red[l] / (i + 1)
-                omp_stm[l] = omp_stm[l] / (i + 1)
+    plt2.savefig(f'./report_plot_SNR_BER_BNP_2.png')
+    plt2.savefig(f'./report_plot_SNR_BER_BNP_2.eps', format='eps', dpi=1200)
+    plt2.close()
 
-                if l < len(itr_label) - 1:
-                    omp_spu[l] = omp_spu[l] / (i + 1)
-                    omp_spu2[l] = omp_spu2[l] / (i + 1)
-
-
-            omp_total[0] = omp_total[0] / ((i + 1)*10)
-            for l in range(1, len(itr_label)):
-                omp_total[l] = omp_total[0] / omp_spu2[l - 1]
-
-            axes[0, 1].plot(itr_label[1:len(itr_label)], omp_itr[1:len(itr_label)], color=color[x], marker=marker[x], label=labels[x], markersize=12)
-            axes[0, 0].plot(itr_label, omp_ber, color=color[x], marker=marker[x], markersize=12)
-            axes[1, 0].semilogy(itr_label, omp_stm, color=color[x], marker=marker[x], markersize=12)
-            axes[1, 1].plot(itr_label[1:len(itr_label)], omp_spu, color=color[x], marker=marker[x], markersize=12)
-            axes[2, 0].semilogy(itr_label, omp_total, color=color[x], marker=marker[x], markersize=12)
-            axes[2, 1].plot(itr_label[1:len(itr_label)], omp_spu2, color=color[x], marker=marker[x], markersize=12)
-
-        a_t[0] = a_t[0] / ((i + 1)*10)
-        for l in range(0, len(itr_label) - 1):
-            spu[l] = spu[l] / (i+1)
-            spu2[l] = spu2[l] / (i+1)
-            # if spu[l] > cores[l]:
-            spu[l] = spu2[l]
-
-        for l in range(1, len(itr_label)):
-            a_t[l] = a_t[0] / spu[l - 1]
-
-        axes[1, 0].plot(itr_label, a_t, color=color[3], marker=marker[3], markersize=12, label=labels[3])
-        axes[1, 1].plot(itr_label[1:len(itr_label)], spu, color=color[3], marker=marker[3], markersize=12)
-
-        for f in range(0, 3):
-            for ff in range(0, 2):
-                axes[f, ff].set_xticklabels(itr_label)
-                axes[f, ff].grid(color='b', ls='-.', lw=0.25)
-                axes[f, ff].patch.set_edgecolor('black')
-                axes[f, ff].patch.set_linewidth('1')
-        axes[0, 1].set_xticklabels(itr_label[1:len(itr_label)])
-        axes[1, 1].set_xticklabels(itr_label[1:len(itr_label)])
-        axes[2, 1].set_xticklabels(itr_label[1:len(itr_label)])
-
-        fig.suptitle("\n\n\n\n\n")
-        fig.legend(bbox_to_anchor=(0.88, 0.97), title="Legend", ncol=4, fontsize=21, title_fontsize=21,
-                   edgecolor='black')
-        if j == 0:
-            plt.savefig(f'./{n}-{qam}_report_plot_BNP.png')
-            plt.savefig(f'./{n}-{qam}_report_plot_BNP.eps', format='eps', dpi=1200)
-            plt.close()
-        else:
-            plt2.savefig(f'./{n}-{qam}_report_plot_BNP.png')
-            plt2.savefig(f'./{n}-{qam}_report_plot_BNP.eps', format='eps', dpi=1200)
-            plt2.close()
-
-    print("\n----------END PLOT RUNTIME--------------\n")
+    print("\n----------END PLOT SNRBER--------------\n")
 
 
 if __name__ == "__main__":
-
     # for start in range(0,10,10):
-    a = np.load(f'./test_result/0_report_plot_{0}_{0+10}_BNP.npz')
-    i = a['i']
-    start = a['start']
-    end = a['end']
-    qrT = a['qrT']
-    asplT = a['asplT']
-    bnp=a['bnp']
-    ber=a['ber']
-    itr=a['itr']
-    plot_bnp(0, i, start, end, qrT, asplT, bnp, ber, itr)
+    plot_bnp()
