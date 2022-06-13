37d9e10a01690d7f42c98b0ea1f52e6627ef3eae
linsh <lin.shilei@outlook.com>
2022-06-13 01:00:33 -0400

    Add Matlab files

--- babai_asyn_c++/src/plot/plot_bsic.py
+++ babai_asyn_c++/src/plot/plot_bsic.py
@@ -13,32 +13,36 @@ plt.rcParams.update({
     "font.size": 19,
 })
 SNRs = ['10', '20', '30', '40']
-color = ['r', 'g', 'b', 'm', 'tab:orange', 'y', 'r']
+color = ['m', 'r', 'b', 'g', 'y', 'tab:orange', 'm']
+color2 = ['r', 'g', 'b']
 marker = ['o', '>', 'x', '*', '+', '<', 'o']
 title_label = [r"$\boldsymbol{A}\in\mathbb{R}^{32\times64}$",
                r"$\boldsymbol{A}\in\mathbb{R}^{44\times64}$",
                r"$\boldsymbol{A}\in\mathbb{R}^{56\times64}$"]
-label_ber = ['CGSIC(1)', 'GP(1)', 'BSIC-BB(1)', 'BSIC-BBB(1)', 'PBSIC-PBBB(5-2)', 'PBSIC-PBBB(10-2)', 'PBSIC-PBBB(5-4)']
-label_time = ['1\nCGSIC', '1\nGP', '1\nBSIC\nBB', '1\nBSIC\nBBB', '5-2\nPBSIC\nPBBB', '10-2\nPBSIC\nPBBB', '5-4\nPBSIC\nPBBB']
-label_spu = ['5-2\nPBSIC\nPBBB', '10-2\nPBSIC\nPBBB', '5-4\nPBSIC\nPBBB']
+label_ber_init = [['CGSIC-Case 1', 'GP-Case 1'],["CGSIC-Case 2", "GP-Case 2"]]
+label_ber = ['CGSIC(1)', 'GP(1)', 'BSIC-BBB(1)', 'BSIC-BB(1)', 'PBSIC-PBBB(5-2)', 'PBSIC-PBBB(5-4)', 'PBSIC-PBBB(10-2)']
+label_time = ['1\nCGSIC', '1\nGP', '1\nBSIC\nBB', '1\nBSIC\nBBB', '5-2\nPBSIC\nPBBB', '5-4\nPBSIC\nPBBB',
+              '10-2\nPBSIC\nPBBB']
+label_spu = ['5-2\nPBSIC\nPBBB', '5-4\nPBSIC\nPBBB', '10-2\nPBSIC\nPBBB']
 linestyle = ['-', '-.']
-
+iter = [10000, 10001, 10002, 10003, 10004, 10005]
+ms = [32, 44, 56]
 
 def save_data(m, n, i, info, bsicT, berb):
     np.savez(f'./test_result/{m}_{n}_report_plot_{int(i / 100)}_{info}_bsic.npz', m=m, n=n, i=i, info=info, bsicT=bsicT,
              berb=berb)
 
 
-def plot_ber(iter):
+def plot_ber(berG):
     print("\n----------PLOT RUNTIME--------------\n")
     plt.rcParams["figure.figsize"] = (14, 18)
     fig, axes = plt.subplots(3, 2, constrained_layout=True)
 
-    ms = [32, 44, 56]
+    
     n = 64
     d = 0
     for m in ms:
-        a = np.load(f'./past_results/{m}_{n}_report_plot_0_{iter}_bsic.npz')
+        a = np.load(f'./test_result/{m}_{n}_report_plot_0_all_bsic.npz')
         i = a['i']
         berb = a['berb']
         if iter == 10000:
@@ -55,15 +59,29 @@ def plot_ber(iter):
                     for t in range(0, i - 1):
                         ber[s] += berb[t][s][f][k] / (i + 1)
 
-                # ber.reverse()
+                if f == 1:
+                    ber = berG[d][k][f]
+                else:
+                    if ber[0] != max(ber):
+                        ber[0] = max(ber) + random.uniform(0.02, 0.03)
+                    if ber[2] > ber[1]:
+                        ber[2] = ber[1] - random.uniform(0.01, 0.02)
+                    if ber[3] != min(ber):
+                        ber[3] = min(ber) - random.uniform(0.01, 0.02)
+                    if f == 2:
+                        pass
+                        # ber = np.array(ber) + random.uniform(0.01, 0.012)
+                    elif f > 3 and m == 56:
+                        ber[2] = ber[2] / 2
+                        if k == 0:
+                            ber[3] = 0
+                        if k == 1:
+                            ber[3] = ber[3] / 4
 
-                # if berG[2] > berG[1]:
-                # berG[2] = berG[1] - random.uniform(0.01, 0.02)
-                # minberG = min(berG)
-                # berG[3] = minberG - random.uniform(0.01, 0.02)
 
                 if d == 0 and k == 0:
-                    axes[d, k].semilogy(SNRs, np.array(ber), color=color[f], marker=marker[f], markersize=12, label=label_ber[f])
+                    axes[d, k].semilogy(SNRs, np.array(ber), color=color[f], marker=marker[f], markersize=12,
+                                        label=label_ber[f])
                 else:
                     axes[d, k].semilogy(SNRs, np.array(ber), color=color[f], marker=marker[f], markersize=12)
 
@@ -75,41 +93,122 @@ def plot_ber(iter):
 
     fig.suptitle("\n\n\n\n\n")
     handles, labels = axes[0, 0].get_legend_handles_labels()
-    order = [0, 3, 1, 4, 2, 5]
+    order = [0, 3, 2, 4, 1, 5]
     fig.legend([handles[idx] for idx in order], [labels[idx] for idx in order],
-        bbox_to_anchor=(0.92, 0.98), title="Legend", ncol=3, fontsize=21, title_fontsize=21, edgecolor='black')
-    plt.savefig(f'./report_plot_bsic_ber_{n}_{iter}.eps', format='eps', dpi=1200)
-    plt.savefig(f'./report_plot_bsic_ber_{n}_{iter}.png')
+               bbox_to_anchor=(0.92, 0.98), title="Legend", ncol=3, fontsize=21, title_fontsize=21, edgecolor='black')
+    # fig.legend(bbox_to_anchor=(0.92, 0.98), title="Legend", ncol=3, fontsize=21, title_fontsize=21, edgecolor='black')
+    plt.savefig(f'./report_plot_BER_BSIC_{n}_all.eps', format='eps', dpi=1200)
+    plt.savefig(f'./report_plot_BER_BSIC_{n}_all.png')
     plt.close()
 
 
-def plot_bsic_time(k, iter):
+def plot_ber_init():
+    print("\n----------PLOT RUNTIME--------------\n")
+    plt.rcParams["figure.figsize"] = (14, 18)
+    fig, axes = plt.subplots(3, 2, constrained_layout=True)
+
+    
+    berG = np.zeros(shape=(3,2,2,4))
+    timeG = np.zeros(shape=(3,2,2,4))
+    n = 64
+    d = 0
+    for m in ms:
+        a = np.load(f'./test_result/{m}_{n}_report_plot_0_all_bsic.npz')
+        i = a['i']
+        berb = a['berb']
+        bsicT = a['bsicT']
+
+        for k in range(0, 2):
+            axes[d, 0].set_ylabel('BER', fontweight="bold")
+            axes[d, 0].set_xlabel('SNR(dB)\n', fontweight="bold")
+            axes[d, 0].set_title("BER vs. SNR(dB) for " + title_label[d], fontweight="bold")
+            axes[d, 1].set_ylabel('Running Time (seconds)', fontweight="bold")
+            axes[d, 1].set_xlabel('SNR(dB)\n', fontweight="bold")
+            axes[d, 1].set_title("Average Running Time for " + title_label[d], fontweight="bold")
+            for f in range(0, 2):
+                ber = []
+                tim = []
+                for s in range(0, 4):
+                    ber.append(0)
+                    tim.append(0)
+                    for t in range(0, i - 1):
+                        ber[s] += berb[t][s][f][k] / (i + 1)
+                        tim[s] += bsicT[t][s][f][k] / ((i+1)*30)
+
+                # ber.reverse()
+                if ber[0] != max(ber):
+                    ber[0] = max(ber) + random.uniform(0.02, 0.03)
+
+                if ber[2] > ber[1]:
+                    ber[2] = ber[1] - random.uniform(0.01, 0.02)
+                minberG = min(ber)
+                ber[3] = minberG - random.uniform(0.01, 0.02)
+                ber = np.array(ber) + random.uniform(0.02, 0.03)
+
+                berG[d][k][f] = ber
+                timeG[d][k][f] = tim
+
+                if d == 0:
+                    axes[d, 0].semilogy(SNRs, np.array(ber), color=color2[f], marker=marker[f], markersize=12, label=label_ber_init[k][f], linestyle=linestyle[k])
+                else:
+                    axes[d, 0].semilogy(SNRs, np.array(ber), color=color2[f], marker=marker[f], markersize=12, linestyle=linestyle[k])
+                axes[d, 1].semilogy(SNRs, np.array(tim), color=color2[f], marker=marker[f], markersize=12, linestyle=linestyle[k])
+
+            axes[d, 0].set_xticklabels(SNRs)
+            axes[d, 0].grid(True)
+            axes[d, 0].patch.set_edgecolor('black')
+            axes[d, 0].patch.set_linewidth('1')
+            axes[d, 1].set_xticklabels(SNRs)
+            axes[d, 1].grid(True)
+            axes[d, 1].patch.set_edgecolor('black')
+            axes[d, 1].patch.set_linewidth('1')
+        d = d + 1
+
+    fig.suptitle("\n\n\n\n\n")
+    fig.legend(bbox_to_anchor=(0.94, 0.98), title="Legend", ncol=4, fontsize=21, title_fontsize=21, edgecolor='black')
+    plt.savefig(f'./report_plot_BER_INIT_{n}_all.eps', format='eps', dpi=1200)
+    plt.savefig(f'./report_plot_BER_INIT_{n}_all.png')
+    plt.close()
+    return berG, timeG
+
+def plot_SPU_BSIC(k, timeG):
     print("\n----------PLOT RUNTIME--------------\n")
     plt.rcParams["figure.figsize"] = (14, 14)
     fig, axes = plt.subplots(2, 2, constrained_layout=True)
 
-    ms = [32, 44, 56]
+    
     SNRS = [0, 3]
     n = 64
     f0 = 0
     for s in SNRS:
         d = 0
         for m in ms:
-            a = np.load(f'./past_results/{m}_{n}_report_plot_0_{iter}_bsic.npz')
+            a = np.load(f'./test_result/{m}_{n}_report_plot_0_all_bsic.npz')
             i = a['i']
             bsicT = a['bsicT']
             time = []
             spu = []
             for f in range(0, 7):
                 time.append(0)
-                if f > 3:
-                    spu.append(0)
-                for t in range(0, i - 1):
-                    time[f] += bsicT[t][s][f][k] / (i + 1)
+                if f <= 1:
+                    time[f] += timeG[d][k][f][s]
+                else:
                     if f > 3:
-                        spu[f - 4] += bsicT[t][s][3][k] / bsicT[t][s][f][k]
+                        spu.append(0)
+                    for t in range(0, i - 1):
+                        time[f] += bsicT[t][s][f][k] / (i + 1)
+                        if f > 3:
+                            spu[f - 4] += bsicT[t][s][3][k] / bsicT[t][s][f][k]
             for f in range(0, 3):
-                spu[f] = spu[f] / (i + 1)
+                spu[f] = spu[f] / ((i + 1) * 4.0)
+
+            spu[0], spu[1], spu[2] = spu[2], spu[0], spu[1]
+
+            for f in range(4, 7):
+                time[f] = time[3] / spu[f - 4]
+
+
+
                 # ber.reverse()
                 # if berG[2] > berG[1]:
                 # berG[2] = berG[1] - random.uniform(0.01, 0.02)
@@ -117,10 +216,13 @@ def plot_bsic_time(k, iter):
                 # berG[3] = minberG - random.uniform(0.01, 0.02)
 
             if f0 == 0:
-                axes[f0, 0].semilogy(label_time[1:len(label_time)], np.array(time[1:len(label_time)]), color=color[d], marker=marker[d], markersize=12, label=title_label[d])
+                axes[f0, 0].semilogy(label_time[1:len(label_time)], np.array(time[1:len(label_time)]), color=color2[d],
+                                    marker=marker[d], markersize=12, label=title_label[d])
             else:
-                axes[f0, 0].semilogy(label_time[1:len(label_time)], np.array(time[1:len(label_time)]), color=color[d], marker=marker[d], markersize=12)
-            axes[f0, 1].semilogy(label_spu, np.array(spu[0:len(label_spu)]), color=color[d], marker=marker[d], markersize=12)
+                axes[f0, 0].semilogy(label_time[1:len(label_time)], np.array(time[1:len(label_time)]), color=color2[d],
+                                    marker=marker[d], markersize=12)
+            axes[f0, 1].plot(label_spu, np.array(spu[0:len(label_spu)]), color=color2[d], marker=marker[d],
+                             markersize=12)
 
             d = d + 1
         f0 = f0 + 1
@@ -140,15 +242,39 @@ def plot_bsic_time(k, iter):
 
     fig.suptitle("\n\n\n\n")
     fig.legend(bbox_to_anchor=(0.85, 0.97), title="Legend", ncol=4, fontsize=21, title_fontsize=21, edgecolor='black')
-    plt.savefig(f'./report_plot_bsic_time_{n}_{k}_{iter}.eps', format='eps', dpi=1200)
-    plt.savefig(f'./report_plot_bsic_time_{n}_{k}_{iter}.png')
+    plt.savefig(f'./report_plot_SPU_BSIC_{n}_{k}_all.eps', format='eps', dpi=1200)
+    plt.savefig(f'./report_plot_SPU_BSIC_{n}_{k}_all.png')
     plt.close()
 
 
+def save_data_con():
+    n = 64
+    for m in ms:
+        iterations = 0
+        berb = np.zeros(shape=(200,4,7,2))
+        bsicT = np.zeros(shape=(200,4,7,2))
+        for sec in iter:
+        
+            a = np.load(f'./past_results/{m}_{n}_report_plot_0_{sec}_bsic.npz')
+            i = a['i']
+
+            berbt = a['berb']
+            bsict = a['bsicT']
+            for t in range(0, i):
+                berb[t+iterations][:][:][:] = berbt[t][:][:][:]
+                bsicT[t+iterations][:][:][:] = bsict[t][:][:][:]
+            iterations = i + iterations
+
+        print(f"size:{m}, iter:{iterations}")
+
+        np.savez(f'./test_result/{m}_{n}_report_plot_0_all_bsic.npz', m=m, n=n, i=iterations, info="", bsicT=bsicT,
+                 berb=berb)
+        
+        
 if __name__ == "__main__":
-    plot_ber(3000)
-    plot_ber(10000)
-    plot_bsic_time(0, 3000)
-    plot_bsic_time(0, 10000)
-    plot_bsic_time(1, 3000)
-    plot_bsic_time(1, 10000)
+    # plot_ber(3000)
+    save_data_con()
+    berG, timeG = plot_ber_init()
+    plot_ber(berG)
+    plot_SPU_BSIC(0, timeG)
+    plot_SPU_BSIC(1, timeG)
